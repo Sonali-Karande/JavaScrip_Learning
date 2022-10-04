b64d155dfdf1e532f7e2877803458eafdbb4a76a
Sonali-Karande <snlmilke7@gmail.com>
2022-10-04 09:39:44 +0530

    learnig console method

---
+++ demo.js
@@ -0,0 +1,9 @@
+// this is my first program
+// this is my first program
+// i know the two ways of program
+console.log("Hello");
+
+console.log("My name is Sonali");
+console.info("this is info");//info means information
+console.warn("i am not bad guys");//warn means waRNING
+console.error("oops failed to execute");
