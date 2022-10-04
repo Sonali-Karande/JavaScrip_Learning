ad6b5247da92610894b863ed5897cb9f74498b65
Sonali-Karande <snlmilke7@gmail.com>
2022-10-04 14:40:42 +0530

    first program execution in js
    first program execution in js

---
+++ ass1.js
@@ -0,0 +1,11 @@
+console.log("Hello Everyone,My name is Sonali Karande\n");
+console.log("My Hobbies are:\n1:Reading\n2:Gardening\n3:Cooking\n");
+console.log("My Strengths are:\n1:Quick Learner\n2:Self disciplined\n3:Self motivated\n");
+console.warn("My Weaknesses are:\n1:I am talkative\n2:Easily trust anyone\n3:Emotional\n");
+console.log("Addition of 30+50:");
+
+console.log(30+50);
+console.log("Multiplication of 49*479:");
+
+console.log(49*479);
+
