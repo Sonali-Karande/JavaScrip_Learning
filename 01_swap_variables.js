9d057955f8605dda0f2e44d35191a2e90fe58310
Sonali-Karande <snlmilke7@gmail.com>
2022-10-06 09:23:40 +0530

    programes on variables
    programes on variables

---
+++ 01_swap_variables.js
@@ -0,0 +1,24 @@
+var my_number=100;
+var your_number=200;
+console.log("before swap");
+console.log(my_number,your_number);
+
+var temp=my_number;
+my_number=your_number;
+your_number=temp;
+console.log("after swap");
+
+console.log(my_number,your_number);
+// console.log(your_number);
+// my_number=your_number
+// console.log(my_number,your_number);
+var num1=100,num2=200,num3=300;
+console.log("Before swap");
+console.log(num1,num2,num3);
+var temp=num1;
+num1=num2;
+num2=num3;
+num3=temp;
+console.log("After swap");
+
+console.log(num1,num2,num3);
---
+++ 01_variables.js
@@ -0,0 +1,8 @@
+var my_apple;//variable declaration
+my_apple = "strawberry";//variable initialization
+var my_name = "sonali";//variable declaration and variable initialization in same line
+var my_name="sonali arjun karande";
+console.log(my_name);
+console.log(my_apple);
+my_apple="200";
+console.log(my_apple);
