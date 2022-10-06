373fdb4d38c60ab7e4869de555c91225906c9abd
Sonali-Karande <snlmilke7@gmail.com>
2022-10-06 15:05:31 +0530

    completed swapping program&variable initialization and declaration
    completed swapping completed swapping program&variable initialization and declarationprogram

---
+++ 01_assignment1.js
@@ -0,0 +1,23 @@
+var myFullName;
+console.log("Logging  before the variable initialization");
+console.log(myFullName);
+console.log("Logging  after the variable initialization");
+
+myFullName="Sonali Karande";
+
+console.log(myFullName);
+console.log("after updating the variable value myFullName");
+
+myFullName="Sonali Arjun Karande";
+console.log(myFullName);
+var myLove="I love only JS";
+console.log(myLove);
+var firstName="Sonali",lastName="Karande",age=31,address="Gurukrupa Bunglow,Dattanagar,Kole,tal-Sangola,Dist-Solapur",pincode="413307"
+,gender="female",married=true;
+console.log("First Name:",firstName);
+console.log("Last Name:",lastName);
+console.log("Address:",address);
+console.log("Pincode:",pincode);
+console.log("Age:",age);
+console.log("Gender:",gender);
+console.log("Married:",married);
---
+++ 01_assignment2.js
@@ -0,0 +1,18 @@
+var sweety="Sweety",cutie="Cutie";
+console.log("Variable Before Swapping");
+console.log(sweety,cutie);
+var temp=sweety;
+sweety=cutie;
+cutie=temp;
+console.log("Variable After Swapping");
+console.log(sweety,cutie);
+var num1=100,num2=200,num3=300;
+console.log("Before swap");
+console.log(num1,num2,num3);
+var temp=num1;
+num1=num2;
+num2=num3;
+num3=temp;
+console.log("After swap");
+
+console.log(num1,num2,num3);
