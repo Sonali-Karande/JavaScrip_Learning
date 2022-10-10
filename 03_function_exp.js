17bfa326baf153710ec19a2a57d869c70a5a6a61
Sonali-Karande <snlmilke7@gmail.com>
2022-10-10 13:07:34 +0530

    completed function expression and string
    completed function expression and string

---
+++ 03_function_exp.js
@@ -0,0 +1,15 @@
+var result=function(arg1,arg2){
+console.log("Function expression",arg1);
+return "FE"
+}
+var returnValue=result(10,20);
+console.log(returnValue);
+var var_name=10;
+console.log(var_name);
+var square=function(value)
+{
+console.log(value*value);
+}
+square(5);
+square(10);
+square(60);
