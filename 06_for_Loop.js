2ae502a076bdddfda493d8c6c7802894cb1ace53
Sonali-Karande <snlmilke7@gmail.com>
2022-10-17 16:06:01 +0530

    class programs on for loop,jump statements and reverse string
    class programs on for loop,jump statements and reverse string

--- 06_for_Loop.js
+++ 06_for_Loop.js
@@ -40,3 +40,4 @@ for (let index = 0; index < 1000; index++) {
     }
 
 }
+
---
+++ 06_revese_string.js
@@ -0,0 +1,8 @@
+var myName="sonali";
+var lenStr=myName.length-1;
+var reverseStr="";
+for (let index = lenStr; index >=0; index--) {
+    console.log(myName.charAt(index));
+reverseStr=reverseStr+myName.charAt(index);
+}
+console.log(reverseStr);
