0df73c15e79606f35dd3f57330da297e59fed2ee
Sonali-Karande <snlmilke7@gmail.com>
2022-10-17 16:04:04 +0530

    completed both the assignments
    completed both the assignments

---
+++ 08_Assignment2.js
@@ -0,0 +1,12 @@
+console.log("*****************************Reverse of string**********************");
+    function reverseString(str) {
+    var lenStr=str.length-1;
+var reverseString="";
+  for (let index =lenStr; index >=0; index--) {
+    reverseString=reverseString+str.charAt(index);
+  }  
+  return reverseString;
+    
+}
+console.log(`Reverse of the string si:${reverseString("Hard work always pays back")}`);
+console.log(`Reverse of the string si:${reverseString("Soon I will be Angular IT Champ")}`);
